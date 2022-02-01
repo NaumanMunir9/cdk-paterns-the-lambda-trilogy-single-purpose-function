6f3005b319a4279d0ca777711b4411b66575fda3
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-01 19:30:38 +0500

    add lambda function

--- lib/single-purpose-function-stack.ts
+++ lib/single-purpose-function-stack.ts
@@ -1,16 +1,20 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class SinglePurposeFunctionStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'SinglePurposeFunctionQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // =============================================================================
+    // add lambda function
+    // =============================================================================
+    const addLambda = new lambda.Function(this, "addLambda", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "add.handler",
+      code: lambda.Code.fromAsset("lambda"),
+    });
   }
 }
